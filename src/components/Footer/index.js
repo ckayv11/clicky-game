220e25a8d92897e4ed5a250d55d6cee327ecfe18
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-07-15 00:04:21 -0500

    added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="bottom">
+            © 2019 Solar System Clicky Game
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
